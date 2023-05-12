e4d0d3f6e90d0b2c093e8494b11c0e99f1cdaa7b
renoagilsaputra87 <renoagilsaputra87@gmail.com>
2023-05-12 15:55:57 +0700

    Update enums.php

--- config/enums.php
+++ config/enums.php
@@ -3,6 +3,26 @@
         'religion' => ['islam','hindu','kristen protestan','kristen Katholik','buddha','khonghucu'],
         'married' => ['menikah','belum menikah'],
         'gender' => ['pria', 'wanita'],
-        'citizenship' => ['wna','wni']
+        'citizenship' => ['wna','wni'],
+        'service_type' => [
+            'Surat keterangan usaha',
+            'Surat keterangan tempat usaha',
+            'Surat keterangan tidak mampu (Sekolah)',
+            'Surat keterangan tidak mampu (umum)',
+            'Surat keterangan miskin',
+            'Surat keterangan penghasilan orang tua',
+            'Surat pengantar SKCK', 
+            'Surat keterangan ahli waris',
+            'Surat keterangan bepergian',
+            'Surat keterangan beda identitas',
+            'Surat Nikah (N1-N6)',
+            'Surat keterangan pernah menikah',
+            'Surat keterangan belum pernah menikah',
+            'Surat keterangan duda/janda',
+            'Surat keterangan sporadik',
+            'Surat keterangan kepemilikan tanah',
+            'Surat keterangan jaminan rumah',
+            'Keterangan ahli waris'
+        ]
     ];
 ?>
